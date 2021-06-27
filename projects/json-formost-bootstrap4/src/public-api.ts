daafe12d60dac2168bc9fd9c00cd254bfd8e909d
Daniel McFarland <dlrm72@gmail.com>
2021-06-27 15:49:40 -0500

    publish bootstrap 0.0.6

--- projects/json-formost-bootstrap4/src/public-api.ts
+++ projects/json-formost-bootstrap4/src/public-api.ts
@@ -2,20 +2,20 @@
  * Public API Surface of json-formost-bootstrap4
  */
 
-export * from './lib/core/util';
-export * from './lib/core/formost-schema-converter.service';
-export * from './lib/core/formost-json-schema-7.interface';
-export * from './lib/core/formost-abstract-control.interface';
-export * from './lib/core/controls/form-group';
-export * from './lib/core/controls/form-array';
-export * from './lib/core/controls/form-control';
-export * from './lib/core/rowify.pipe';
-export * from './lib/core/controlify.pipe';
+export * from './lib/core';
+// export * from './lib/core/formost-schema-converter.service';
+// export * from './lib/core/formost-json-schema-7.interface';
+// export * from './lib/core/formost-abstract-control.interface';
+// export * from './lib/core/controls/form-group';
+// export * from './lib/core/controls/form-array';
+// export * from './lib/core/controls/form-control';
+// export * from './lib/core/rowify.pipe';
+// export * from './lib/core/controlify.pipe';
 //export * from './lib/core/util';
 
 // export * from './lib/core';
-export * from './lib/json-formost-form/json-formost-form.component';
-export * from './lib/json-formost-control/json-formost-control.component';
 export * from './lib/json-formost-array/json-formost-array.component';
+export * from './lib/json-formost-control/json-formost-control.component';
+export * from './lib/json-formost-form/json-formost-form.component';
 export * from './lib/json-formost-group/json-formost-group.component';
 export * from './lib/json-formost-bootstrap4.module';
